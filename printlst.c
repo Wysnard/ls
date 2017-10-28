9f39808f461d770cd3d31652956401239ff32f62
Wysnard <Wysnard@users.noreply.github.com>
2017-10-28 18:55:12 +0200

    re

--- printlst.c
+++ printlst.c
@@ -12,25 +12,15 @@ void	ft_filetype(struct dirent *dir)
 void	ft_getst(info *inf)
 {
 	struct	stat	*buf;
-
-	if (inf->dir->d_type == 10)
-		lstat(inf->dir->d_name, buf);
-	else
-		stat(inf->dir->d_name, buf);
-	inf->st = statcpy(buf);
-}
-
-void	ft_getst2(info *inf)
-{
-	struct	stat	*buf;
 	char		str[10];
 
 	if (inf->dir->d_type == 10)
 		lstat(inf->dir->d_name, buf);
 	else
 		stat(inf->dir->d_name, buf);
-	ft_strmod(buf->st_mode, str);
-	ft_printf("%s %d ", str, buf->st_nlink);
+	inf->st = statcpy(buf);
+	ft_strmod(inf->st->st_mode, str);
+	ft_printf("%s %d ", str, inf->st->st_nlink);
 }
 
 void	ft_total(t_list	*list)
@@ -72,7 +62,6 @@ void	ft_printlst(t_list *list, char *flag)
 		{
 			inf = (info *)list->content;
 			ft_filetype(inf->dir);
-			ft_getst2(inf);
 			ft_uid(inf->st);
 			ft_gid(inf->st);
 			ft_printf("%d ", inf->st->st_size);
