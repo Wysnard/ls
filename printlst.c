9cda1032c5185559601147f1235e649e82ca9646
Wysnard <Wysnard@users.noreply.github.com>
2017-10-28 15:48:28 +0200

    return to buf

--- printlst.c
+++ printlst.c
@@ -12,18 +12,18 @@ void	ft_filetype(struct dirent *dir)
 void	ft_getl(info *inf)
 {
 	char		str[10];
-	//struct	stat	*buf;
+	struct	stat	*buf;
 
 	if (inf->dir->d_type == 10)
-		lstat(inf->dir->d_name, inf->st);
+		lstat(inf->dir->d_name, buf);
 	else
-		stat(inf->dir->d_name, inf->st);
+		stat(inf->dir->d_name, buf);
 	ft_filetype(inf->dir);
-	ft_strmod(inf->st->st_mode, str);
-	ft_printf("%s %d ", str, inf->st->st_nlink);
-	ft_uid(inf->st);
-	ft_gid(inf->st);
-	ft_printf("%d ", inf->st->st_size);
+	ft_strmod(buf->st_mode, str);
+	ft_printf("%s %d ", str, buf->st_nlink);
+	ft_uid(buf);
+	ft_gid(buf);
+	ft_printf("%d ", buf->st_size);
 }
 
 void	ft_printlst(t_list *list, char *flag)
