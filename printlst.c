1b924c933378de5118cfb2c82460c93f5ea51aa0
Wysnard <Wysnard@users.noreply.github.com>
2017-10-29 01:33:37 +0200

    try

--- printlst.c
+++ printlst.c
@@ -44,8 +44,8 @@ void	ft_getst(struct dirent *dir)
 	else
 		stat(dir->d_name, buf);
 	//ft_mod(inf);
-	ft_uid(buf);
-	//ft_gid(buf);
+	//ft_uid(buf);
+	ft_gid(buf);
 	ft_printf("%d ", buf->st_size);
 }
 
