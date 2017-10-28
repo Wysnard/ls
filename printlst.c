6aa6980173aa6ef50b05493b70fd26f39809a812
Wysnard <Wysnard@users.noreply.github.com>
2017-10-28 17:30:53 +0200

    u

--- printlst.c
+++ printlst.c
@@ -31,17 +31,17 @@ void	ft_total(t_list	*list)
 {
 	t_list	*tmp;
 	info	*inf;
-	int	total;
+	unsigned int	total;
 
 	tmp = list;
 	total = 0;
 	while (tmp)
 	{
 		inf = (info *)tmp->content;
-		total += inf->st->st_blksize;
+		total += inf->st->st_blocks;
 		tmp = tmp->next;
 	}
-	ft_printf("total %d\n", total);
+	ft_printf("total %u\n", total);
 }
 
 void	ft_printlst(t_list *list, char *flag)
