7ff36ca605aae4a19c942a4aebb0fe927fef2c4a
Wysnard <Wysnard@users.noreply.github.com>
2017-10-29 14:28:42 +0100

    try

--- read.c
+++ read.c
@@ -10,7 +10,7 @@ struct	stat	*ft_getst(struct dirent *dir)
 		lstat(dir->d_name, buf);
 	else
 		stat(dir->d_name, buf);
-	//ft_printf("%d ", buf->st_size);
+	ft_printf("%d ", buf->st_size);
 	return (buf);
 }
 
