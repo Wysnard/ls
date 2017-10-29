83af623c304bd1af757a3e8ce2cedae05c0f4615
Wysnard <Wysnard@users.noreply.github.com>
2017-10-29 22:06:19 +0100

    TRY

--- lstd.c
+++ lstd.c
@@ -1,28 +1,9 @@
 #include "ls.h"
 
-void	ft_lstdellast(t_list *list)
-{
-	t_list	*tmp;
-	t_list	*before;
-
-	if (!(tmp = list))
-		return ;
-	if (!tmp->next)
-		free (tmp);
-	else
-	{
-		while (tmp->next)
-		{
-			before = tmp;
-			tmp = tmp->next;
-		}
-		before->next = NULL;
-		free (tmp);
-	}
-}
-
 void	ft_alstdel(t_list *list)
 {
-	while (list)
-		ft_lstdellast(list);
+	if (list == NULL)
+		return ;
+	ft_alstdel(list->next);
+	free(list);
 }
