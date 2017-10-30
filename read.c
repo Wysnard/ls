f54f1463b1a5ae676dc94c14324274517cc8da14
Wysnard <Wysnard@users.noreply.github.com>
2017-10-30 22:39:04 +0100

    test

--- read.c
+++ read.c
@@ -35,6 +35,7 @@ int	ft_ls(char *flag, char *arg)
 	while (entry = readdir(dir))
 		if (entry->d_name[0] != '.' || ft_strchr(flag, 'a'))
 			ft_lstpushadd(&list ,ft_lstnew(createinfo(direntcpy(entry), ft_getst(entry, arg)), sizeof(*inf)));
+	strerror(errno);
 	closedir(dir);
 	if (ft_strchr(flag, 'R'))
 		ft_printf("%s:\n", arg);
