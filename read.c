b3d113e8fbab8dfb5e98d0a8f8f9c350e86ba191
Wysnard <Wysnard@users.noreply.github.com>
2017-10-30 21:00:52 +0100

    test

--- read.c
+++ read.c
@@ -6,9 +6,9 @@ struct	stat	*ft_getst(struct dirent *dir)
 
 	if (!(buf = (struct stat *)malloc(sizeof(struct stat))))
 		exit (EXIT_FAILURE);
-	if (dir->d_type == DT_LNK)
+	/*if (dir->d_type == DT_LNK || dir->d_type == DT_UNKNOWN)
 		lstat(dir->d_name, buf);
-	else
+	else*/
 		stat(dir->d_name, buf);
 	return (buf);
 }
