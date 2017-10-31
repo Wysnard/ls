6245eaf78b9ee41c276d9deacbfd8ba2f6df7b05
Wysnard <Wysnard@users.noreply.github.com>
2017-10-31 20:31:27 +0100

    l

--- read.c
+++ read.c
@@ -40,7 +40,6 @@ int	ft_ls(char *flag, char *arg)
 		ft_printf("%s:\n", arg);
 	if (ft_strchr(flag, 'r'))
 		ft_lstrev(&list);
-	ft_swap(&list->content, &list->next->content);
 	ft_printlst(list, flag, arg);
 	tmp = list;
 	if (ft_strchr(flag, 'R'))
